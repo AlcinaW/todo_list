2787edf75c5a098bf54ea1089402456bba07a1bd
AlcinaW <white_kaitou@hotmail.com>
2015-10-14 17:57:05 -0400

    Updated to-do app
    Completed delete function, still need to add ng-disabled state.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var app = angular.module('todoApp', []);
 
 function TodoCtrl() {
+  this.todo;
   this.todos = [
     {text: 'learn angular', done: false},
     {text: 'write the content for the next module', done: false},
-    {text: 'buy cheese', done: true}
+    {text: 'buy cheese', done: true},
   ];
   
   this.filters = [
@@ -16,11 +17,20 @@ function TodoCtrl() {
 
 app.controller('TodoCtrl', TodoCtrl);
 
-// TodoCtrl.prototype.addTodo = function(){
-// 	var AddedtoDo = {
-// 			text: " ", done: false
-// 		};
-// 	this.todo.push(AddedtoDo);
-// };
+TodoCtrl.prototype.addTodo = function(){
+  console.log(this.NewtoDo);
+	var AddedtoDo = { 
+    text: this.NewtoDo,
+    done: false 
+		};
+	this.todos.push(AddedtoDo);
+};
 
-
+TodoCtrl.prototype.clearComplete = function(){
+  for (i=this.todos.length-1; i>=0; i--){
+    // console.log(i + " " + this.todos[i].done);
+    if (this.todos[i].done === true) {
+      this.todos.splice(i, 1);
+    }
+  }
+};
